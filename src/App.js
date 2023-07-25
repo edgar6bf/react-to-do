9e0f28bea383cbe4b76fc59e8604abbe85ac0988
edgar6bf <edgar6bf@gmail.com>
2023-07-26 02:42:03 +0900

    feat: 리스트 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
     <div>
-
+      <TodoList></TodoList>
     </div>
   );
 }
